2bd2be561392d8efc3bd8307497d0a54838111da
相 <79968329+b0829065@users.noreply.github.com>
2022-04-07 20:29:47 +0800

    Update main.c

--- qz3/main.c
+++ qz3/main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+
 void rec_dec(char *s){
     if (!*s) printf ("\n");
     else {
         printf("%c,",*s);
-        return rec_dec(s + 1);
+        rec_dec(s + 1);
     }
 }
+
 void hanoi_tower(int i ,char A, char B, char C ) {
+    FILE* fp;
+    fp = fopen("hanoi.txt","a");
     if (i == 1){
-        FILE* fp;
-        fp = fopen("hanoi.txt","a");
         fprintf(fp,"Move disk 1 form %c to %c\n", A, C);
-        fclose(fp);
     }
     else {
         hanoi_tower(i-1, A, C, B);
         hanoi_tower(1, A, B, C);
         hanoi_tower(i-1, B, A, C);
     }
+    fclose(fp);
 }
+
 int multiplication(int i,int j){
     if (i == 10) {
         return 0;    
@@ -29,7 +32,6 @@ int multiplication(int i,int j){
         multiplication(i, j+1);
     }
 }
-
 
 int main()
 {
