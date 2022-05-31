77e036481f5f5877e41435e663e3e1a3cc84db38
相 <79968329+b0829065@users.noreply.github.com>
2022-05-31 15:03:09 +0800

    Update main.cpp

--- qz6/main.cpp
+++ qz6/main.cpp
@@ -1,63 +1,65 @@
-#include<iostream>
+#include <iostream>
 #include<string>
-
 using namespace std;
 
-class Clock {
-	public:
-		int hr, min, sec;
-		string mer;
-		Clock(int hr_, int min_, int sec_, string mer_) {
-			hr = hr_;
-			min = min_;
-			sec = sec_;
-			mer = mer_;
-		}
-		display() {
-			cout << hr << ":" << min << ":" << sec << " " << mer << endl;
-		}
-}; 
-
-class StandardClock : Clock {
-	public:
-		StandardClock(int a, int b, int c, string d) : Clock(a, b, c, d){
-			hr = a;
-			min = b;
-			sec = c;
-			mer = d;
-		}
-		display() {
-			if (mer == "AM") {
-				cout << "上午 " << hr << ":" << min << ":" << sec << endl;
-			} else if (mer == "PM") {
-				cout << "下午 " << hr << ":" << min << ":" << sec << endl;
-			}
-		}
+class Clock{
+    public:
+        int hour;
+        int minute;
+        int second;
+        string meridian;
+        Clock(int h, int m, int s, string Mil){
+            hour = h;
+            minute = m;
+            second = s;
+            meridian = Mil;
+        }
+        void display(){
+            cout << hour << ":" << minute << ":" << second << " " << meridian << endl;
+        }
 };
 
-class MilitaryClock : Clock {
-	public:
-		MilitaryClock(int a, int b, int c, string d) : Clock(a, b, c, d){
-			hr = a;
-			min = b;
-			sec = c;
-			mer = d;
-		}
-		display() {
-			if (mer == "AM") {
-				cout << hr << ":" << min << ":" << sec << endl;
-			} else if (mer == "PM") {
-				cout << hr+12 << ":" << min << ":" << sec << endl;
-			}
-		}
+class StandardClock : public Clock{
+    public:
+        StandardClock(int h, int m, int s, string Mil) : Clock(h,m,s,Mil){
+            hour = h;
+			minute = m;
+			second = s;
+			meridian = Mil;
+        }
+        display(){
+            if(meridian == "AM"){
+                cout << "早上 " << hour << ":" << minute << ":" << second << meridian;
+            } else if (meridian == "PM") {
+                cout << "下午 " << hour << ":" << minute << ":" << second << meridian;
+            }
+        }
 };
 
-int main() {
+class MilitaryClock : public Clock{
+    public:
+        MilitaryClock(int h, int m, int s, string Mil) : Clock(h,m,s,Mil){
+            hour = h;
+			minute = m;
+			second = s;
+			meridian = Mil;
+        }
+        display(){
+			if (meridian == "AM") {
+				cout << hour << ":" << minute << ":" << second << endl;
+			} else if (meridian == "PM") {
+				cout << hour+12 << ":" << minute << ":" << second << endl;
+			}
+        }
+};
+
+int main()
+{
 	Clock cc(10, 25, 43, "AM");
 	StandardClock sc(10, 25, 43, "AM");
 	MilitaryClock mc(10, 25, 43, "PM");
 	cc.display();
 	sc.display();
 	mc.display();
-	return 0;
+    return 0;
 }
